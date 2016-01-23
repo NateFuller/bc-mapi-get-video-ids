10e50347c75ececc9df8bcace3b5af386240e50f
Nathan Fuller <fatenuller@gmail.com>
2016-01-23 17:55:48 -0500

    Successfully obtaining all video IDs

--- getVideoIds.js
+++ getVideoIds.js
@@ -1,19 +1,5 @@
 var http = require('http');
 
-if (process.argv.length !== 4) {
-	console.log("USAGE: node getVideoIds.js [publisher_id] [mapi_read_token]");
-	process.exit();
-}
-
-var pub_id = process.argv[2]
-var token = process.argv[3]
-var item_count = -1;
-var page_count = -1;
-
-var ITEMS_PER_PAGE = 100;
-
-getItemCount();
-
 function getItemCount(callback) {
 	var options = {
 		host: 'api.brightcove.com',
@@ -22,28 +8,95 @@ function getItemCount(callback) {
 
 	http.get(options, function(resp) {
 		resp.setEncoding('utf8');
-		var str = ""
+		var str = ''
 		resp.on('data', function(chunk) {
 			str += chunk
 		});
 		resp.on('end', function() {
 			var resp_json = JSON.parse(str);
-			item_count = resp_json.total_count;
-			page_count = Math.ceil(item_count/ITEMS_PER_PAGE);
-			console.log("ITEMS RETURNED: " + resp_json.total_count);
-			console.log("PAGE COUNT: " + Math.ceil(page_count));
+			var item_count = resp_json.total_count;
+			var page_count = Math.ceil(item_count/ITEMS_PER_PAGE);
+			callback(item_count, page_count)
 		});
-	}).on("error", function(e) {
-		console.log("Got error: " + e.message);
+	}).on('error', function(e) {
+		console.log('Got error: ' + e.message);
 		process.exit();
 	});
 
 }
 
-function getVideoIds(num_items, callback) {
-	var options = {
-		host: 'api.brightcove.com',
-		path: '/services/library?command=search_videos&page_size=100&video_fields=id&page_number=0&token='
+function getVideoIds() {
+
+	getItemCount(function(item_count, page_count) {
+		console.log('ITEMS RETURNED: ' + item_count);
+		console.log('PAGE COUNT: ' + page_count);
+		var options = {
+			host: 'api.brightcove.com',
+			path: '/services/library?command=search_videos&page_size=' + ITEMS_PER_PAGE + '&video_fields=id&token=' + token
+		}
+		var video_ids = [];
+
+		var urls = [];
+		var responses = [];
+		var completed_requests = 0;
+
+		for (var page_number = 0; page_number < page_count; page_number++) {
+			urls.push({host:options.host, path:options.path + '&page_number=' + page_number});
+		}
+
+		for (url in urls) {
+			var req = http.request(urls[url], function(res) {
+				// console.log('STATUS: ' + res.statusCode);
+				// console.log('HEADERS: ' + JSON.stringify(res.headers));
+				res.setEncoding('utf8');
+				var str = ''
+				res.on('data', function(chunk) {
+					str += chunk;
+				});
+				res.on('end', function() {
+					for (video_id in JSON.parse(str).items)
+						video_ids.push(JSON.parse(str).items[video_id].id)
+					completed_requests++;
+					if (completed_requests == urls.length) {
+						console.log("all done!");
+						console.log("Number of video IDs returned: " + video_ids.length);
+						testVideoIdsAreUnique(video_ids);
+					}
+				});
+			});
+
+			req.on('error', function(e) {
+				console.log('problem with request: ' + e.message);
+			});
+
+			req.end();
+		}
+	});	
+}
+
+function testVideoIdsAreUnique(video_ids) {
+	var sorted_arr = video_ids.sort();
+	var duplicates = [];
+	for (var i = 0; i < sorted_arr.length - 1; i++) {
+		if (sorted_arr[i + 1] === sorted_arr[i]) {
+			duplicates.push(sorted_arr[i]);
+		}
+	}
+	if (duplicates.length > 0) {
+		console.log("Duplicates found: ", duplicates);
+	} else {
+		console.log("All video IDs are unique!");
 	}
 }
 
+if (process.argv.length !== 4) {
+	console.log('USAGE: node getVideoIds.js [publisher_id] [mapi_read_token]');
+	process.exit();
+}
+
+var pub_id = process.argv[2]
+var token = process.argv[3]
+
+var ITEMS_PER_PAGE = 100;
+
+getVideoIds();
